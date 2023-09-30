fbd54f7211ca3f6274ab03598177b2d5e1a633f9
Kevin Semilla <kevinsemilla@gmail.com>
2023-09-30 12:12:13 +1000

    Add env variables

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000"
+export const API_URL = import.meta.env.VITE_API_URL
 
 export const UserRoleChoices: { label: string; value: string }[] = [
   { label: "User", value: "user" },
