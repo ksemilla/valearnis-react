633a55521b95308ad8804231a245b47a203337d8
Kevin Semilla <kevinsemilla@gmail.com>
2023-09-30 18:48:39 +1000

    Add ui to quiz navigation

--- src/containers/AdminQuizzesContainer.tsx
+++ src/containers/AdminQuizzesContainer.tsx
@@ -1,19 +1,34 @@
 import { Box, Button, HStack } from "@chakra-ui/react"
-import { Link, Outlet, useParams } from "react-router-dom"
+import { Link, Outlet, useLocation, useParams } from "react-router-dom"
 
 export default function AdminQuizzesContainer() {
   const { slug } = useParams()
+  const [_, quizResource, quizSub] = useLocation()
+    .pathname.split("/")
+    .slice(3, 6)
 
   return (
     <Box>
       <HStack>
-        <Button as={Link} to={`/admin/lessons/${slug}`}>
+        <Button
+          as={Link}
+          to={`/admin/lessons/${slug}`}
+          backgroundColor={!quizResource && !quizSub ? "gray.300" : "gray.100"}
+        >
           Back to {slug}
         </Button>
-        <Button as={Link} to={`/admin/lessons/${slug}/quizzes`}>
+        <Button
+          as={Link}
+          to={`/admin/lessons/${slug}/quizzes`}
+          backgroundColor={quizResource && !quizSub ? "gray.300" : "gray.100"}
+        >
           Quizzes
         </Button>
-        <Button as={Link} to={`/admin/lessons/${slug}/quizzes/create`}>
+        <Button
+          as={Link}
+          to={`/admin/lessons/${slug}/quizzes/create`}
+          backgroundColor={quizSub === "create" ? "gray.300" : "gray.100"}
+        >
           Create Quiz
         </Button>
       </HStack>
