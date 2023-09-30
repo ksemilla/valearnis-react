918b1833f594fbc873b6001c879a7135b902b7b9
Kevin Semilla <kevinsemilla@gmail.com>
2023-09-30 19:06:43 +1000

    Change auth container routes

--- src/router.tsx
+++ src/router.tsx
@@ -28,28 +28,33 @@ import AdminQuizzesContainer from "./containers/AdminQuizzesContainer"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<AuthContainer />}>
+    <Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
-      <Route element={<BaseContainer />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/my-account" element={<MyAccount />} />
-        <Route path="/:slug" element={<LessonDetail />} />
-        <Route path="/:slug/:id" element={<QuizDetail />} />
-        <Route path="quiz-results" element={<QuizAnswerList />} />
-        <Route path="quiz-results/:id" element={<QuizAnswerDetail />} />
-      </Route>
-      <Route element={<AdminContainer />}>
-        <Route path="/admin/users" element={<UserList />} />
-        <Route path="/admin/users/:id" element={<UserDetail />} />
-        <Route path="/admin/lessons" element={<AdminLessonList />} />
-        <Route path="/admin/lessons/create" element={<AdminLessonCreate />} />
+      <Route element={<AuthContainer />}>
+        <Route element={<BaseContainer />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/my-account" element={<MyAccount />} />
+          <Route path="/:slug" element={<LessonDetail />} />
+          <Route path="/:slug/:id" element={<QuizDetail />} />
+          <Route path="quiz-results" element={<QuizAnswerList />} />
+          <Route path="quiz-results/:id" element={<QuizAnswerDetail />} />
+        </Route>
+        <Route element={<AdminContainer />}>
+          <Route path="/admin/users" element={<UserList />} />
+          <Route path="/admin/users/:id" element={<UserDetail />} />
+          <Route path="/admin/lessons" element={<AdminLessonList />} />
+          <Route path="/admin/lessons/create" element={<AdminLessonCreate />} />
 
-        <Route path="/admin/lessons/:slug" element={<AdminQuizzesContainer />}>
-          <Route path="" element={<AdminLessonEdit />} />
-          <Route path="quizzes" element={<AdminQuizList />} />
-          <Route path="quizzes/create" element={<AdminQuizCreate />} />
-          <Route path="quizzes/:id" element={<AdminQuizDetail />} />
+          <Route
+            path="/admin/lessons/:slug"
+            element={<AdminQuizzesContainer />}
+          >
+            <Route path="" element={<AdminLessonEdit />} />
+            <Route path="quizzes" element={<AdminQuizList />} />
+            <Route path="quizzes/create" element={<AdminQuizCreate />} />
+            <Route path="quizzes/:id" element={<AdminQuizDetail />} />
+          </Route>
         </Route>
       </Route>
     </Route>
